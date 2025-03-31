f0b16eaf8e78dff068df733fa185a5ab3157e515
vini-151 <ovini.15515@gmail.com>
2025-03-31 09:47:33 -0300

    crivo e aula4

--- lista2/ex7.py
+++ lista2/ex7.py
@@ -1,1 +1,12 @@
-prime = [True for i in range(num+1)]
+num = int(input("Digite um número: "))
+
+primos = [True] * (num + 1)
+
+primos[0:2] = [False, False]
+
+for i in range(2, int((num**0.5)) + 1):
+    if primos[i]:
+        for j in range(i * i, num + 1, i):
+            primos[j] = False
+
+print([i for i, primo in enumerate(primos) if primo])
--- lista2/ex9.py
+++ lista2/ex9.py
@@ -1,9 +1,7 @@
 num = input("Digite um número: ")
-
 contador = 0
 
 for i in num: contador = contador + int(i)**3
 
 if contador == int(num): print(f"{num} é número de Armstrong")
 else: print(f"{num} não é número de Armstrong")
-
